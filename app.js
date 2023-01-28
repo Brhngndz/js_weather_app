8ebc41ce5dce23d478df854d4dcc1c52ddd813b6
brhngndz <brhnn.gndz@gmail.com>
2023-01-28 23:13:33 +0300

    sidebar added

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ sehir.addEventListener("keypress", setQuery);
 
 const getResult = (sehirIsmi) => {
   let query = `${url}weather?q=${sehirIsmi}&appid=${key}&units=metric&lang=tr`;
-  console.log(query);
 
   let x = "url('https://source.unsplash.com/1920x1080/?" + sehirIsmi + "')";
   document.body.style.backgroundImage = x;
+
   fetch(query)
     .then((weather) => {
       return weather.json();
@@ -31,24 +31,39 @@ const displayResult = (result) => {
   deg.innerText = `${Math.round(result.main.temp)}°c`;
 
   if (`${Math.round(result.main.temp)}` > 24) {
-    document.body.children[0].children[0].children[1].children[1].style.color =
+    document.body.children[0].children[0].children[2].children[1].style.color =
       "#FE7701";
   }
   if (`${Math.round(result.main.temp)}` < 21) {
-    document.body.children[0].children[0].children[1].children[1].style.color =
+    document.body.children[0].children[0].children[2].children[1].style.color =
       "#FFD573";
   }
   if (`${Math.round(result.main.temp)}` < 11) {
-    document.body.children[0].children[0].children[1].children[1].style.color =
+    document.body.children[0].children[0].children[2].children[1].style.color =
       "#95D9F2";
   }
   if (`${Math.round(result.main.temp)}` < 1) {
-    document.body.children[0].children[0].children[1].children[1].style.color =
+    document.body.children[0].children[0].children[2].children[1].style.color =
       "#3BAFE5";
   }
 
   let durum = document.querySelector(".durum");
   durum.innerText = `${result.weather[0].description} `;
+
+  let wind1 = document.querySelector(".wind1");
+  wind1.innerText = `Degree: ${result.wind.deg}° `;
+
+  let wind2 = document.querySelector(".wind2");
+  wind2.innerText = `Speed: ${result.wind.speed} Kts`;
+
+  let like = document.querySelector(".like");
+  like.innerText = `${result.main.feels_like} °c`;
+
+  let hTemp = document.querySelector(".hTemp");
+  hTemp.innerText = `${Math.ceil(result.main.temp_max)} °c`;
+
+  let lTemp = document.querySelector(".lTemp");
+  lTemp.innerText = `${Math.floor(result.main.temp_min)} °c`;
 
   let img = document.querySelector(".img");
   const icon = result.weather[0].icon;
@@ -56,12 +71,16 @@ const displayResult = (result) => {
 
   let objectDate = new Date();
   let day = objectDate.getDate();
-  console.log(day); // 23
+
   let month = objectDate.getMonth();
-  console.log(month + 1); // 8
+
   let year = objectDate.getFullYear();
-  console.log(year); // 2022
 
   let zaman = document.querySelector(".time");
   zaman.innerText = `${day}/${month + 1}/${year}`;
+};
+
+let sidebar = document.getElementById("sidebar");
+document.getElementById("sidebar-btn").onclick = function () {
+  sidebar.classList.toggle("fade");
 };
